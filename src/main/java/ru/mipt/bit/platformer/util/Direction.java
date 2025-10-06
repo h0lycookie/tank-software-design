0d6ccfc2084929c58b7a0ce8c10bf8099d399996
Ilichev Kirill <kirill.iliych@gmail.com>
2025-10-06 07:11:51 +0300

    introduce vector2 in direction

--- src/main/java/ru/mipt/bit/platformer/util/Direction.java
+++ src/main/java/ru/mipt/bit/platformer/util/Direction.java
@@ -4,45 +4,27 @@ import java.util.Collection;
 import java.util.List;
 
 import com.badlogic.gdx.math.GridPoint2;
+import com.badlogic.gdx.math.Vector2;
 
 public enum Direction {
-    UP(90f, List.of(com.badlogic.gdx.Input.Keys.UP, com.badlogic.gdx.Input.Keys.W)) {
-        @Override
-        public GridPoint2 getNewPosition(GridPoint2 currentPosition) {
-            return GdxGameUtils.incrementedY(currentPosition);
-        }
-    },
+    UP(new Vector2(0, 1), 90f, List.of(com.badlogic.gdx.Input.Keys.UP, com.badlogic.gdx.Input.Keys.W)),
+    DOWN(new Vector2(0, -1), -90f, List.of(com.badlogic.gdx.Input.Keys.DOWN, com.badlogic.gdx.Input.Keys.S)),
+    LEFT(new Vector2(-1, 0), -180f, List.of(com.badlogic.gdx.Input.Keys.LEFT, com.badlogic.gdx.Input.Keys.A)),
+    RIGHT(new Vector2(1, 0), 0f, List.of(com.badlogic.gdx.Input.Keys.RIGHT, com.badlogic.gdx.Input.Keys.D));
 
-    DOWN(-90f, List.of(com.badlogic.gdx.Input.Keys.DOWN, com.badlogic.gdx.Input.Keys.S)) {
-        @Override
-        public GridPoint2 getNewPosition(GridPoint2 currentPosition) {
-            return GdxGameUtils.decrementedY(currentPosition);
-        }
-    },
-
-    LEFT(-180f, List.of(com.badlogic.gdx.Input.Keys.LEFT, com.badlogic.gdx.Input.Keys.A)) {
-        @Override
-        public GridPoint2 getNewPosition(GridPoint2 currentPosition) {
-            return GdxGameUtils.decrementedX(currentPosition);
-        }
-    },
-
-    RIGHT(0f, List.of(com.badlogic.gdx.Input.Keys.RIGHT, com.badlogic.gdx.Input.Keys.D)) {
-        @Override
-        public GridPoint2 getNewPosition(GridPoint2 currentPosition) {
-            return GdxGameUtils.incrementedX(currentPosition);
-        }
-    };
-
+    private final Vector2 direction;
     private final float rotation;
     private final Collection<Integer> keys;
 
-    Direction(float rotation, Collection<Integer> keys) {
+    Direction(Vector2 direction, float rotation, Collection<Integer> keys) {
+        this.direction = direction;
         this.rotation = rotation;
         this.keys = keys;
     }
 
-    public abstract GridPoint2 getNewPosition(GridPoint2 currentPosition);
+    public GridPoint2 getNewPosition(GridPoint2 currentPosition) {
+        return currentPosition.cpy().add((int) direction.x, (int) direction.y);
+    }
 
     public float getRotation() {
         return rotation;
