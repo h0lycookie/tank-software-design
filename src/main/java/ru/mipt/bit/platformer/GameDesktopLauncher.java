5cec1d0d49489ffc2bb803f64ac5754c96240dda
Ilichev Kirill <kirill.iliych@gmail.com>
2025-12-22 03:06:53 +0300

    stable

--- src/main/java/ru/mipt/bit/platformer/GameDesktopLauncher.java
+++ src/main/java/ru/mipt/bit/platformer/GameDesktopLauncher.java
@@ -4,48 +4,24 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Interpolation;
 
 import ru.mipt.bit.platformer.util.TileMovement;
-import ru.mipt.bit.platformer.entity.RenderBehavior;
-import ru.mipt.bit.platformer.entity.MovingRenderBehavior;
-import ru.mipt.bit.platformer.entity.TankGraphics;
-import ru.mipt.bit.platformer.entity.TankModel;
-import ru.mipt.bit.platformer.entity.TreeGraphics;
-import ru.mipt.bit.platformer.entity.TreeModel;
-import ru.mipt.bit.platformer.entity.interfaces.CollidableEntity;
-import ru.mipt.bit.platformer.entity.interfaces.MovableEntity;
-import ru.mipt.bit.platformer.entity.interfaces.RenderableEntity;
 import ru.mipt.bit.platformer.config.Config;
-import ru.mipt.bit.platformer.entity.HealthBarDecorator;
-import ru.mipt.bit.platformer.entity.HealthBarModel;
 import ru.mipt.bit.platformer.entity.MapState;
-import ru.mipt.bit.platformer.entity.MoveBehavior;
 import ru.mipt.bit.platformer.field.Mover;
 import ru.mipt.bit.platformer.field.Renderer;
-import ru.mipt.bit.platformer.level.LevelGenerator;
-import ru.mipt.bit.platformer.level.LevelGeneratorRandom;
 import ru.mipt.bit.platformer.util.AIControlHandler;
 import ru.mipt.bit.platformer.util.ControlHandler;
 import ru.mipt.bit.platformer.util.MoveEntityCommandGenerator;
-import ru.mipt.bit.platformer.util.ObjectType;
 
 import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
 import static ru.mipt.bit.platformer.util.GdxGameUtils.*;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class GameDesktopLauncher implements ApplicationListener {
     private static String levelFilePath = "./src/main/resources/level_design.txt";
@@ -60,8 +36,6 @@ public class GameDesktopLauncher implements ApplicationListener {
     private Renderer renderer;
     private ControlHandler controlHandler;
     private AIControlHandler aiControlHandler;
-
-    // private MovableEntity playerTank;
 
     @Override
     public void create() {
--- src/main/java/ru/mipt/bit/platformer/util/Direction.java
+++ src/main/java/ru/mipt/bit/platformer/util/Direction.java
@@ -7,19 +7,17 @@ import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.Vector2;
 
 public enum Direction {
-    UP(new Vector2(0, 1), 90f, List.of(com.badlogic.gdx.Input.Keys.UP, com.badlogic.gdx.Input.Keys.W)),
-    DOWN(new Vector2(0, -1), -90f, List.of(com.badlogic.gdx.Input.Keys.DOWN, com.badlogic.gdx.Input.Keys.S)),
-    LEFT(new Vector2(-1, 0), -180f, List.of(com.badlogic.gdx.Input.Keys.LEFT, com.badlogic.gdx.Input.Keys.A)),
-    RIGHT(new Vector2(1, 0), 0f, List.of(com.badlogic.gdx.Input.Keys.RIGHT, com.badlogic.gdx.Input.Keys.D));
+    UP(new Vector2(0, 1), 90f),
+    DOWN(new Vector2(0, -1), -90f),
+    LEFT(new Vector2(-1, 0), -180f),
+    RIGHT(new Vector2(1, 0), 0f);
 
     private final Vector2 direction;
     private final float rotation;
-    private final Collection<Integer> keys;
 
-    Direction(Vector2 direction, float rotation, Collection<Integer> keys) {
+    Direction(Vector2 direction, float rotation) {
         this.direction = direction;
         this.rotation = rotation;
-        this.keys = keys;
     }
 
     public GridPoint2 getNewPosition(GridPoint2 currentPosition) {
@@ -28,9 +26,5 @@ public enum Direction {
 
     public float getRotation() {
         return rotation;
-    }
-
-    public boolean isKeyPressed() {
-        return this.keys.stream().anyMatch(com.badlogic.gdx.Gdx.input::isKeyPressed);
     }
 }
